045bcebeb048a0cd43bed7b4115303efb508a729
Kenna <kennaalastair@gmail.com>
2019-11-01 10:13:31 -0700

    DELETE project and PUT project

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -25,7 +25,7 @@ router.get('/:id', (req, res) => {
 router.post("/", (req, res) => {
     const { name, description } = req.body;
     if (!name || !description) {
-        return res.status(400).json({ error: "Need title and contents" });
+        return res.status(400).json({ error: "Project must include a name and description" });
     }
     db.insert({ name, description })
         .then(id => {
@@ -34,7 +34,38 @@ router.post("/", (req, res) => {
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({ error: "Error inserting post" });
+            res.status(500).json({ error: "Error inserting project to database" });
+        });
+});
+
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, description } = req.body;
+
+    db.update(id, { name, description })
+        .then(updated => {
+            res.status(200).json(updated);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "Error updating project" });
+        });
+});
+
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+
+    db.remove(id)
+        .then(removed => {
+            if (removed) {
+                res.status(204).end();
+            } else {
+                res.status(404).json({ error: `Project with id ${id} does not exist` });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "Error deleting project" });
         });
 });
 
