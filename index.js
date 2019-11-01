8f5d125f17aa0475a1e9bc75d495639f6fd95198
Kenna <kennaalastair@gmail.com>
2019-11-01 09:01:07 -0700

    created server

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+const server = require('./server.js');
+
+server.listen(6000, () => console.log('server listening on port 6000'));
